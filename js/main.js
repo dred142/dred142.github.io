9f2ed37eacace4e6d9fab7370430fc9b85ebe78e
Tretyak Vyacheslaw <tretyakslavik@mail.ru>
2019-07-18 17:42:57 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function($) {
+
+    // Добавляем маску для поля с номера телефона
+    $('#phone').mask('+380(99)999-99-99');
+    // Проверяет отмечен ли чекбокс согласия
+    // с обработкой персональных данных
+    $('#check').on('click', function() {
+        if ($("#check").prop("checked")) {
+            $('#button').attr('disabled', false);
+        } else {
+            $('#button').attr('disabled', true);
+        }
+    });
+
+    // Отправляет данные из формы на сервер и получает ответ
+    $('#contactForm').on('submit', function(event) {
+        
+        event.preventDefault();
+
+        var form = $('#contactForm'),
+            button = $('#button'),
+            answer = $('#answer'),
+            loader = $('#loader');
+
+        $.ajax({
+            url: 'mail.php',
+            type: 'POST',
+            data: form.serialize(),
+            beforeSend: function() {
+                answer.empty();
+                button.attr('disabled', true).css('margin-bottom', '20px');
+                loader.fadeIn();
+            },
+            success: function(result) {
+                loader.fadeOut(300, function() {
+                    answer.text(result);
+                });
+                form.find('.form-control').val(' ');
+                button.attr('disabled', false);
+            },
+            error: function() {
+                loader.fadeOut(300, function() {
+                    answer.text('Произошла ошибка! Попробуйте позже.');
+                });
+                button.attr('disabled', false);
+            }
+        });
+    });
+});
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1500);
+    return false;
+});
+$(document).ready(function(){
+    $('#nav-menu').click(function(){
+        $('ul.nav-list').addClass('nav-open').slideToggle('300');
+    });
+});
