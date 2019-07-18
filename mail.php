a6d0c3ae9921411a367fb666a006c0152e10024c
Tretyak Vyacheslaw <tretyakslavik@mail.ru>
2019-07-18 17:34:21 +0300

    New project. Landing page
    Новый заказ на лендинг.
    8 блоков.
    Цена: 1200 грн.
    Срок: 1 день.
    Адаптивный шаблон на Bootstrap 4.

---
+++ mail.php
@@ -0,0 +1,13 @@
+﻿<?php
+if(isset( $_POST['phone']))
+$phone = $_POST['phone'];
+if(isset( $_POST['area']))
+$area = $_POST['area'];
+$subject = "dredinvest@gmail.com";
+
+$content="Новая заявка!\n Номер: $phone \n Район: $area";
+$recipient = "slavatret98@gmail.com";
+$mailheader = "Новая заявка! \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Ошибка!");
+echo "Отправлено!";
+?>
